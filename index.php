ca528072567f2443dff554a1e25a900446f84a51
Bjorn Stange <bjorn.stange@grooveshark.com>
2013-05-12 23:18:45 -0400

    well this is pretty simple

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<center><title>URL Shortener</title>
+<h1>URL Shortener</h1>
 <form name="URL" action="shortener.php" method="get">
 URL: <input type="text" name="URL">
 <input type="submit" value="Submit">
-</form>
+</form></center>
 <?php
 ?>
--- shortener.php
+++ shortener.php
@@ -16,9 +16,36 @@ if (isset($_GET["URL"])) {
 
     $query->execute();
     $query->close();
+    $urlID = base_convert($con->insert_id, 10, 36);
+    $returnURL = $_SERVER["HTTP_HOST"] . "/$urlID";
+    ?>
+    <script language="JavaScript">
+      function selectText(textField) 
+      {
+        textField.focus();
+        textField.select();
+      }
+    </script>
+    <?php
+
+    echo "<center><h1>Shortened URL</h1><input type=\"text\" name=\"returnURL\" size=\"100\" value=$returnURL onClick='selectText(this);'></center>";
 }
 
-if (isset($_GET["code"])) {
-    echo htmlspecialchars($_GET["code"]);
+elseif (isset($_GET["code"])) {
+    // decided to switch up the query operation here :3
+    $urlID = base_convert($_GET["code"], 36, 10);
+    $result = mysqli_query($con, "SELECT URL FROM url_mapping WHERE url_id=$urlID");
+    $retArr = mysqli_fetch_array($result);
+    if(isset($retArr['URL'])) {
+        $redirect = $retArr['URL'];
+        header( "Location:$redirect");
+    }
+    else {
+        echo "not a valid url ID!";
+    }
+    // echo print_r($con, TRUE);
+}
+else {
+    header( 'Location: /');
 }
 ?>
